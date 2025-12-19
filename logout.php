aa02890597d8ae7f738f8f93f26370584403a293
trantrungloc <loc570574@gmail.com>
2025-12-19 23:36:15 +0700

    mới sửa lại login

--- logout.php
+++ logout.php
@@ -4,5 +4,5 @@ Session::init();
 
 unset($_SESSION['user_login'], $_SESSION['user_id'], $_SESSION['user_name'], $_SESSION['user_email']);
 
-header('Location: login.php');
+header('Location: /html/index.php');
 exit;
